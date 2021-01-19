ef445df7dd631f6c0468bd7f84c72ee6b530efcd
gokulrajrz <rajgokul9847161393@gmail.com>
2021-01-19 08:52:07 +0530

    a

--- src/main.js
+++ src/main.js
@@ -20,10 +20,41 @@ export default function (Vue, {
     href: 'https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900',
   });
 
-  const opts = {} //opts includes, vuetify themes, icons, etc.
+  const opts = {
+    theme: {
+      themes: {
+        light: {
+          primary: "#14C6FF",
+          secondary: "#424242",
+          accent: "#82B1FF",
+          error: "#FF5252",
+          info: "#2196F3",
+          success: "#4CAF50",
+          warning: "#FFC107",
+          lightblue: "#14c6FF",
+          yellow: "#FFCF00",
+          pink: "#FF1976",
+          orange: "#FF8657",
+          magenta: "#C33AFC",
+          darkblue: "#1E2D56",
+          gray: "#909090",
+          neutralgray: "#9BA6C1",
+          green: "#2ED47A",
+          red: "#FF5c4E",
+          darkblueshade: "#308DC2",
+          lightgray: "#BDBDBD",
+          lightpink: "#FFCFE3",
+          white: "#FFFFFF",
+          btnorange: "#fa5b2f"
+        }
+      }
+    }
+  } //opts includes, vuetify themes, icons, etc.
   Vue.use(Vuetify)
 
   appOptions.vuetify = new Vuetify(opts);
+
+
 
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,17 +1,53 @@
 <template>
   <Layout>
-    <v-carousel hide-delimiters>
+    <v-carousel hide-delimiters :height="`${height}px`">
       <v-carousel-item
         v-for="(item, i) in items"
         :key="i"
         :src="item.src"
       ></v-carousel-item>
     </v-carousel>
-    <v-hover>
-      <v-btn x-large color="#fa5b2f" dark class="btnShop px-15">
+    <v-hover v-slot="{ hover }">
+      <v-btn
+        x-large
+        :color="hover ? '#fff' : '#fa5b2f'"
+        dark
+        class="btnShop px-15"
+        :class="hover ? 'btnorange--text' : 'white--text'"
+      >
         SHOP NOW
       </v-btn>
     </v-hover>
+    <v-container class="d-flex flex-column pa-0 align-center">
+      <h2 class="mt-10">Featured</h2>
+      <v-layout row wrap justify-space-around class="layout mt-5 mb-2">
+        <v-flex
+          xs12
+          sm6
+          md4
+          class="flx d-flex justify-center align-center"
+          v-for="(product , i) in products"
+          :key="i"
+        >
+          <v-hover v-slot="{ hover }">
+            <v-card
+              elevation="2"
+              outlined
+              :height="hover ? '95%' : '92%'"
+              :width="hover ? '95%' : '92%'"
+              class="trans mt-3"
+            >
+              <v-img
+                height="100%"
+                width="100%"
+                src="https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg"
+              ></v-img>
+            </v-card>
+          </v-hover>
+        </v-flex>
+      </v-layout>
+    </v-container>
+    <v-footer outlined padless> dfgdgh </v-footer>
   </Layout>
 </template>
 
@@ -37,7 +73,33 @@ export default {
           src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
         },
       ],
+      products: [
+        {
+          src: "~/img/6.jpg",
+        },
+        {
+          src: "~/img/6.jpg",
+        },
+        {
+          src: "~/img/6.jpg",
+        },
+        {
+          src: "~/img/6.jpg",
+        },
+        {
+          src: "~/img/6.jpg",
+        },
+        {
+          src: "~/img/6.jpg",
+        },
+      ],
+      height: 100,
     };
+  },
+  mounted() {
+    let appBar = 64;
+    this.height = screen.height - appBar - 140;
+    // console.log(this.height);
   },
 };
 </script>
@@ -45,8 +107,20 @@ export default {
 <style>
 .btnShop {
   position: absolute;
-  margin-top: -100px;
+  margin-top: -150px;
   left: 50%;
   transform: translateX(-50%);
+  transition: 0.3s;
+  border-radius: 0;
+  letter-spacing: 5px;
+}
+.layout {
+  width: 100%;
+}
+.flx {
+  height: 350px;
+}
+.trans {
+  transition: 0.2s;
 }
 </style>
